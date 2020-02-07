fe8c9ba9bbe452a96a49235c667a117ad4f7de74
Ryan Seit <ryan@ryanseit.com>
2020-02-07 19:10:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 		:railroads => {
       :names => {:name1 => "", :name2 => "", :name3 => "", :name4 => ""},
       :pieces => 4,
-			:rent_in_dollars => {:one_piece_owned => 25, :key2 => "Two", :key3 => "Three", :key4 => "Four"}
+			:rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :key3 => "Three", :key4 => "Four"}
     }
 	}
 end
